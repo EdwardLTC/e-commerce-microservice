8ae9d7a3f7afc01fd0cd843d3c9468985502eaef
EdwardLTC <lethanhcong06062003@gmail.com>
2025-03-11 13:05:23 +0700

    Add Dockerfile and .dockerignore; update .gitignore and main.ts for logging

--- nest-api-gateway-auth/src/main.ts
+++ nest-api-gateway-auth/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,4 +9,6 @@ async function bootstrap() {
   return app;
 }
 
-bootstrap().then((app) => {});
+bootstrap().then(async app => {
+  Logger.debug(`API Gateway is running on: ${await app.getUrl()} - ${process.env.NODE_ENV}`, 'Bootstrap');
+});
