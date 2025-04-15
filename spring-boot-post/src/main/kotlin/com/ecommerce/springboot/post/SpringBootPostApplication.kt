9883e2d9a241aec27133b0407e1f7000a52d2a50
EdwardLTC <lethanhcong06062003@gmail.com>
2025-04-15 13:47:05 +0700

    init post service

---
+++ spring-boot-post/src/main/kotlin/com/ecommerce/springboot/post/SpringBootPostApplication.kt
@@ -0,0 +1,11 @@
+package com.ecommerce.springboot.post
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootPostApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootPostApplication>(*args)
+}
---
+++ spring-boot-post/src/test/kotlin/com/ecommerce/springboot/post/SpringBootPostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ecommerce.springboot.post
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootPostApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
